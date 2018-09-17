2c8a9c67bb8ea64f57fc2b994edbb649ded21dee
RamonFontana <fontanaramon@gmail.com>
2018-09-17 11:36:47 -0300

    corrigido model

--- lms-django/lms_app/models.py
+++ lms-django/lms_app/models.py
@@ -1,12 +1,8 @@
 from django.db import models
-<<<<<<< HEAD
-import diciplina
-import professor
-import diciplinaOfertada
-=======
+
 import lms_app.diciplina
 import lms_app.professor
 import lms_app.diciplinaOfertada
->>>>>>> 42c08dabfa67537656cc74f1d93c3a69777101f3
+
 
 # Create your models here. 
