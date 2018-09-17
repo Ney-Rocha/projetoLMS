04edabad1b31e88f96306fb4841a176bb7d6ef5b
RamonFontana <fontanaramon@gmail.com>
2018-09-17 11:06:04 -0300

    separado classes nos respectivos arquivos (professor, diciplina e diciplinaOfertada)

---
+++ lms-django/lms_app/diciplina.py
@@ -0,0 +1,12 @@
+class Disciplina(models.Model):
+
+    def save(self):
+
+        if len(Disciplina.objects.filter(nome=self.nome)) > 0:
+            raise Exception("Disciplina já cadastrada!")
+
+        super(Disciplina,self).save()
+
+    nome = models.TextField(max_length=50)
+    ementa = models.TextField(max_length=5000)
+
