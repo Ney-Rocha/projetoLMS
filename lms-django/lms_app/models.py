42c08dabfa67537656cc74f1d93c3a69777101f3
RamonFontana <fontanaramon@gmail.com>
2018-09-17 11:25:47 -0300

    crorrigido caminhos dos metodos

--- lms-django/lms_app/models.py
+++ lms-django/lms_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-import diciplina
-import professor
-import diciplina
+import lms_app.diciplina
+import lms_app.professor
+import lms_app.diciplinaOfertada
 
-# Create your models here.
+# Create your models here. 
